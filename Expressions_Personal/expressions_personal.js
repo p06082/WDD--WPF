53c4f48c6451283ad3c6a57241bb16665acf3128
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 19:11:11 -0500

    tried variation
    tried to input 24h time but this provides incorrect hours as well.

--- Expressions_Personal/expressions_personal.js
+++ Expressions_Personal/expressions_personal.js
@@ -9,10 +9,10 @@
 //Calculation for projected hours of sleep
 
 //Ask user the time they will go to bed and the time they plan to wake up the next morning
-var timeToBed = prompt("What time will you go to bed tonight?");
+var timeToBed = prompt("What time will you go to bed tonight? 24h time");
 console.log(timeToBed);
 
-var timeWaking = prompt("What time will you be waking up tomorrow?");
+var timeWaking = prompt("What time will you be waking up tomorrow? 24h time");
 console.log(timeWaking)
 
 var hoursSlept = timeWaking - timeToBed;
