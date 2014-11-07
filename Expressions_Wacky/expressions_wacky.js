ab7037860f0b80a656a4ca1d749e470d6474cfa2
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 20:21:01 -0500

    wacky initial code
    added comment explaining code gist and first prompt

--- Expressions_Wacky/expressions_wacky.js
+++ Expressions_Wacky/expressions_wacky.js
@@ -5,3 +5,8 @@
 */
 
 //alert("Javescript Works");
+
+//Calculation for Massive Picnic - how many blankets needed for massive blanket to put on a football field.
+
+var fieldLength = prompt("What is the length of the football field?");
+console.log(fieldLength);
