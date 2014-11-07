e0f9fa43b9a9f68bf031568d23388db7ee68afa0
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 19:02:29 -0500

    added comments and initial code
    comments for code description and code for user input about bedtime

--- Expressions_Personal/expressions_personal.js
+++ Expressions_Personal/expressions_personal.js
@@ -6,3 +6,8 @@
 
 //alert("Javescript Works");
 
+//Calculation for projected hours of sleep
+
+//Ask user the time they will go to bed and the time they plan to wake up the next morning
+var timeToBed = prompt("What time will you go to bed tonight?");
+console.log(timeToBed);
