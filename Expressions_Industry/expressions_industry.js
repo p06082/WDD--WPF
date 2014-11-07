c958ac72df9a46c6d59a2859d94eb9ddf48b6da4
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 21:15:58 -0500

    industry setup
    description for code and part one

--- Expressions_Industry/expressions_industry.js
+++ Expressions_Industry/expressions_industry.js
@@ -5,3 +5,9 @@
 */
 
 //alert("Javescript Works");
+
+//Hours to code site at a rate of three hours per page
+
+//Ask user to give total pages on the site
+var totalPages = prompt("How many pages does this site contain?");
+console.log(totalPages);
