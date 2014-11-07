8cd2ed5f1069307a7b0f4d2d0e889b177def2b6e
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 21:25:04 -0500

    added greeting, fixed sentence
    added greeting in prompt and added word “hours” in alert

--- Expressions_Industry/expressions_industry.js
+++ Expressions_Industry/expressions_industry.js
@@ -8,12 +8,12 @@
 
 //Hours to code site at a rate of three hours per page
 
-//Ask user to give total pages on the site
-    var totalPages = prompt("How many pages does this site contain?");
+//Greet user and prompt to give total pages on the site
+    var totalPages = prompt("Hello, let's begin figuring out how many hours you'll need to work before this site is finished. How many pages does this site contain?");
     console.log(totalPages);
 
 //Calculation for hours of work
     var hoursOfWork = totalPages * 3;
 
 //Solution
-    alert("You will need to work "+hoursOfWork+ " to finish this site.");
+    alert("You will need to work "+hoursOfWork+ " hours to finish this site.");
