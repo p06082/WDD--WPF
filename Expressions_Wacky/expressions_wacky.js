21446a5547fb4a8d7503fc4ff0c2fe2a59f015d1
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 20:30:17 -0500

    added comments for clarification, part two
    part two of blanket, field expression added.

--- Expressions_Wacky/expressions_wacky.js
+++ Expressions_Wacky/expressions_wacky.js
@@ -8,10 +8,20 @@
 
 //Calculation for Massive Picnic - how many blankets needed for massive blanket to put on a football field.
 
-var fieldLength = prompt("What is the length of the football field? (inches)");
-console.log(fieldLength);
+//Football Field Area
+    var fieldLength = prompt("What is the length of the football field? (inches)");
+    console.log(fieldLength);
 
-var fieldWidth = ("What is the width of the football field? (inches)");
-console.log(fieldWidth);
+    var fieldWidth = ("What is the width of the football field? (inches)");
+    console.log(fieldWidth);
 
-var fieldArea = fieldLength * fieldWidth;
+    var fieldArea = fieldLength * fieldWidth;
+
+//Blanket Area
+     var blanketLength = prompt("What is the length of the blanket? (inches)");
+    console.log(blanketLength);
+
+    var blanketWidth = ("What is the width of the blanket? (inches)");
+    console.log(blanketWidth);
+
+    var blanketArea = blanketLength * blanketWidth;
