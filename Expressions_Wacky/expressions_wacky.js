72d565abf85ddb16e08f09c196aefe32623ef89c
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 20:25:37 -0500

    part one of wacky expression finished
    added code for calculating football field area

--- Expressions_Wacky/expressions_wacky.js
+++ Expressions_Wacky/expressions_wacky.js
@@ -8,5 +8,10 @@
 
 //Calculation for Massive Picnic - how many blankets needed for massive blanket to put on a football field.
 
-var fieldLength = prompt("What is the length of the football field?");
+var fieldLength = prompt("What is the length of the football field? (inches)");
 console.log(fieldLength);
+
+var fieldWidth = ("What is the width of the football field? (inches)");
+console.log(fieldWidth);
+
+var fieldArea = fieldLength * fieldWidth;
