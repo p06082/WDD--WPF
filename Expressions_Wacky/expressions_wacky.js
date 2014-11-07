89fea7c168a29731cb4626917bd69ed92d1ae1b8
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 21:33:30 -0500

    added greeting, changes
    added extra information before questions, changed you to we

--- Expressions_Wacky/expressions_wacky.js
+++ Expressions_Wacky/expressions_wacky.js
@@ -9,7 +9,7 @@
 //Calculation for Massive Picnic - how many blankets needed for massive blanket to put on a football field.
 
 //Football Field Area
-    var fieldLength = prompt("What is the length of the football field? (inches)");
+    var fieldLength = prompt("It's time once again for the Annual Football Field Picnic. Let's see      how many blankets we will need to sew this year's blanket. What is the length of the football      field? (inches)");
     console.log(fieldLength);
 
     var fieldWidth = prompt("What is the width of the football field? (inches)");
@@ -18,16 +18,15 @@
      var fieldArea = fieldLength * fieldWidth;
 
 //Blanket Area
-     var blanketLength = prompt("What is the length of the blanket? (inches)");
+     var blanketLength = prompt("What is the length of the blanket template? (inches)");
     console.log(blanketLength);
 
-    var blanketWidth = prompt("What is the width of the blanket? (inches)");
+    var blanketWidth = prompt("What is the width of the blanket template? (inches)");
     console.log(blanketWidth);
 
     var blanketArea = blanketLength * blanketWidth;
 
 //Blankets Needed
+    var blanketsTotal = fieldArea / blanketArea;
 
-var blanketsTotal = fieldArea / blanketArea;
-
-alert("You will need "+blanketsTotal+" blankets to make one massive blanket for this football field.");
+    alert("We will need "+blanketsTotal+" blankets to make one massive blanket for this football       field.");
