ae465589c2122f6a747a5a88f3b61bdc7cf98509
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 20:43:23 -0500

    realized missing piece
    prompt was missing from two lines of code. code now functions as
    expected.

--- Expressions_Wacky/expressions_wacky.js
+++ Expressions_Wacky/expressions_wacky.js
@@ -12,14 +12,14 @@
     var fieldLength = prompt("What is the length of the football field? (inches)");
     console.log(fieldLength);
 
-    var fieldWidth = ("What is the width of the football field? (inches)");
+    var fieldWidth = prompt("What is the width of the football field? (inches)");
     console.log(fieldWidth);
 
 //Blanket Area
      var blanketLength = prompt("What is the length of the blanket? (inches)");
     console.log(blanketLength);
 
-    var blanketWidth = ("What is the width of the blanket? (inches)");
+    var blanketWidth = prompt("What is the width of the blanket? (inches)");
     console.log(blanketWidth);
 
     var fieldArea = fieldLength * fieldWidth;
