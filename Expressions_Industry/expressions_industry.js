17f8c679e705f4da112b23a8bb9ba052c3a82c41
LaToya Morgan <latoya.jeanette@gmail.com>
2014-11-06 21:21:26 -0500

    finished code
    code finished and functioning as expected

--- Expressions_Industry/expressions_industry.js
+++ Expressions_Industry/expressions_industry.js
@@ -9,5 +9,11 @@
 //Hours to code site at a rate of three hours per page
 
 //Ask user to give total pages on the site
-var totalPages = prompt("How many pages does this site contain?");
-console.log(totalPages);
+    var totalPages = prompt("How many pages does this site contain?");
+    console.log(totalPages);
+
+//Calculation for hours of work
+    var hoursOfWork = totalPages * 3;
+
+//Solution
+    alert("You will need to work "+hoursOfWork+ " to finish this site.");
